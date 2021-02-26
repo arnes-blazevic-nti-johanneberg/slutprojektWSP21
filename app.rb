817b5622099493bb0574de29dfe18ac86c0111a9
arnes-blazevic-nti-johanneberg <60173033+arnes-blazevic-nti-johanneberg@users.noreply.github.com>
2021-02-26 14:32:50 +0100

    vecka 8
    jobbat med app.br och slimfilerna som handlar om att logga in och kunna registerera sig

--- app.rb
+++ app.rb
@@ -1,15 +1,46 @@
-require "sinatra"
-require "slim"
-require "sqlite3"
-require "bcrypt"
-require "byebug"
-require_relative 'model.rb'
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+require './model.rb'
 
 enable :sessions
 
-db = SQLite3::Database.new("db/slutprojekt.db")
-db.results_as_hash = true
-
 get('/') do
-    slim(:register)
+  slim(:index)
+end
+
+get('/showlogin') do #inte skapat denna ännu men det ska vara dit man kommer/behörigheten som man får om man lyckats logga in
+  slim(:login)
+end
+
+post('/login') do
+  username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new('db/slutprojekt.db')
+  db.results_as_hash = true
+  result = db.execute('SELECT * FROM users WHERE username = ?', username).first
+  pwdigest = result['password']
+  id = result['id']
+
+  if BCrypt::Password.new(password) == password
+    session[:id] = id
+    redirect('/hem') # inte klar med denna delen ännu, #inte skapat denna ännu men det ska vara dit man kommer/behörigheten som man får om man lyckats logga in
+  else
+    "Du har inte angett rätt lösenord"
   end
+
+end
+post('/register') do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+  if (password == password_confirm)
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.execute("INSERT INTO users (username,password) VALUES (?,?)", username, password)
+    redirect("/hem") #inte klar denna delen ännu, #inte skapat denna ännu men det ska vara dit man kommer/behörigheten som man får om man lyckats logga in
+  else
+    "Lösenordet matchade inte!"
+  end
+end
--- model.rb
+++ model.rb
@@ -1,7 +1,5 @@
-module Model
-
-    def db()
-        db = SQLite3::Database.new("db/slutprojekt.db")
-        db.results_as_hash = true
-        return db
-    end
+def db()
+    db = SQLite3::Database.new("db/slutprojekt.db")
+    db.results_as_hash = true
+    return db
+end
