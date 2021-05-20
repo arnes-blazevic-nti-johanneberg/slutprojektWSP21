62a23de3f736731a8095fb2362b40a2e62d2d781
arnes-blazevic-nti-johanneberg <60173033+arnes-blazevic-nti-johanneberg@users.noreply.github.com>
2021-05-20 11:52:06 +0200

    final changes

--- app.rb
+++ app.rb
@@ -6,14 +6,15 @@ require './model.rb'
 
 enable :sessions
 
-
+# This is a important function thath checks before you do anything if yout seasion id or your request path is allowed to proceed further into the webbsite.
+# This function is important becouse it makes it much harder to hack yoursekf throuth the webbsites with help of the browser, before block blocks this method
 before do 
   if (session[:id] ==  nil) && (request.path_info != '/') && (request.path_info != '/showlogin' && (request.path_info != '/showregister') && (request.path_info != '/login') && (request.path_info != '/users/new'))
     redirect("/")
   end
 end
 
-
+# If something goes wrong in the website it slims a error website that explain what went wrong
 get("/error") do
   slim(:error)
 end
@@ -27,7 +28,9 @@ end
 #  end
 #end
 
-
+# This is the first website that show when opening the website
+#it checks if you already are loged in and if you are you are redirected to "books_read" sinde you already have your seasion id
+#if you are not already logged in then it slims and sends you to "index" wher you either may loggin or register a account
 get ('/') do
     if already_logged_in() == true
         db = connect_to_db("db/slutprojekt.db")
@@ -39,7 +42,7 @@ get ('/') do
     end
 end
 
-
+# checks if you have a youser Id and if you do it sends you to the main page ("books_read"), and if you dont habe a seasion id it redirects you to and slims user/login, where you have to login and get a seasion id to be able to enter the rest of the website
 get('/showlogin') do #inte skapat denna ännu men det ska vara dit man kommer/behörigheten som man får om man lyckats logga in
   if (session[:id] !=  nil)
     redirect("books_read")
@@ -47,11 +50,15 @@ get('/showlogin') do #inte skapat denna ännu men det ska vara dit man kommer/be
     slim(:"users/login")
   end
 end
-
+# Slims and sends you to and displays the main page where alla your books are shown for you
 get('/homepage') do 
   slim(:"index",locals:{books_read:result})
 end
 
+
+# Attempts to login a user.
+# if users inputed password matches with the databases it gets you your seasion:id and you may proced further in the website and gets redirected to ("books_read")
+# else you will get redirected to ("/") where you may either login or create an account to gain a seasion:id
 post('/login') do
   #if session[:now]
     #time = session[:now].split("_")
@@ -76,12 +83,15 @@ post('/login') do
   end
 end
 
-
+# Attempts to logout a user.
+#
+# Trieys to destroy the users seasion which becouse of my before do funktions thath checks if seasion id = nill, therefore it kicks you out from the side to the "/"
 get("/logout") do
   session.destroy
   redirect("/")
 end
-
+# Checks if you you already have a seassion id, if it is not nill it sends you to the main page ("books_read")
+# Else it slims a users/register where you may register and get a seassion id and mover further into the webbsite
 get("/showregister") do
   if (session[:id] !=  nil)
     redirect("books_read")
@@ -90,6 +100,7 @@ get("/showregister") do
   end
 end
 
+# Attempts to register a user.
 post('/users/new') do
   username = params[:username]
   password = params[:password]
@@ -97,6 +108,9 @@ post('/users/new') do
   register(username, password, password_confirm)
 end
 
+# pots all the books thath is owned by the user (session[:id]), you may at this page only see your own books
+# if your seasion id works, it gets all of the books thath belonges to the user from the database
+# else it just redirect the user to ("/") wher you either have to register or login to gain an seasion:id and be able to proceed furter into the webbsite
 get("/books_read") do
   if  id = session[:id].to_i #kopplar till databas och hämtar info om vem som är inloggad
     puts "test"
@@ -112,7 +126,7 @@ get("/books_read") do
 end
 
 
-
+# Deletes books, only able to delete the books that belong to your user_id
 post('/books_read/delete') do
   id = params[:number]  # skapa en if sats om man kommer in med rätt inlgogning
   user_id = session[:id].to_i
@@ -120,6 +134,7 @@ post('/books_read/delete') do
   redirect('/books_read')
 end
 
+# makes new books, only able to make new books that will belong to your user_id
 post('/books_read/new') do
   content = params[:content]
   genre_id = params[:genre_id]              # Gjort model.rb på denna
@@ -128,7 +143,7 @@ post('/books_read/new') do
   new_post(content, user_id, genre_id)
   redirect('/books_read')
 end
-
+# edits your already owned books, you may only edit the name and not the genre, if you want to change the genre you unfortenetly have to delete the book and make a new one
 post('/books_read/edit') do
   content = params[:content]
   id = params[:number]
@@ -137,6 +152,8 @@ post('/books_read/edit') do
   redirect('/books_read')
 end
 
+
+# gets all of the genres thath are listen in the database and posts the on the webbsite
 get("/books/findbooks/index") do
   db = SQLite3::Database.new('db/slutprojekt.db')
   db.results_as_hash = true
@@ -144,7 +161,7 @@ get("/books/findbooks/index") do
   slim(:"books/findbooks/index",locals:{genre:result})
 end
 
-
+# allows you to chose which genre you want to search for and find new books about to read, you get to see all of the books thath have the same genre thath other users have read
 post("/books/findbooks/index") do
   genre_id = params[:genre_name]
   session[:genre_id] = genre_id
@@ -154,6 +171,7 @@ post("/books/findbooks/index") do
   redirect("/books/findbooks:genre_id") #jag vill nog bli redirectat till ny sida som visar all denna infon med vald informaiton
 end
 
+# based on the genre_id it posts all of the books in the database that have matching genre_id with the users chosen one
 get("/books/findbooks:genre_id") do #jag vill skapa en ny sida som visar vald genre från förra sidan
   genre_id = session[:genre_id]                                    #Gjort model på denna
   result = find_new_books_genres()
--- model.rb
+++ model.rb
@@ -1,38 +1,43 @@
+# connects to database
+# @param [sting] connects the path to the database and get you the right information
 def connect_to_db(path)
     db = SQLite3::Database.new(path)
     db.results_as_hash = true
     return db
 end
-
+# Updates your posts
+# @param [String] content the books name based on what the user puts in
+# @param [String] book id every book gets an book id
 def update_post(content, id)
     db = connect_to_db("db/slutprojekt.db")
     db.execute("UPDATE books_read SET content = ? WHERE id = ?", content, id)
 end
 
-
+# Deletes books
+#
+# @param [integrer] book id the chosen object will be deleted based on the id (object) chosen by the user, it is aswll locked to the user_id
+# @param [string] changes the name of the book, only the user may do thath
 def delete_post(id)
     db = connect_to_db("db/slutprojekt.db")
     db.execute("DELETE FROM books_read WHERE id = ?", id)
 end
 
-
+# Makes new books
+#
+# @param [String] content the books name based on what the user puts in
+# @param [String] genre_id every book gets an genre id based on the selection by the user
+# @param [String] user_id every book when crafted gets a uniquq user_id that diffences the book from every other book 
 def new_post(content, user_id, genre_id)
     db = connect_to_db("db/slutprojekt.db")
     db.execute("INSERT INTO books_read (content, user_id, genre_id) VALUES (?,?,?)", content, user_id, genre_id)
 end
 
 
-
-
-
-def login(username, password)
-        db = SQLite3::Database.new("db/slutprojekt.db")
-        db.results_as_hash = true
-        result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-        return result
-end
-
-
+# Attempts to register a user.
+#
+# @param [String] username The username inputted by the user.
+# @param [String] password The password inputted by the user.
+# @param [String] password_confirm The password_confirm inputted by the user, and checks if the inputed password matches with the inputed password_digest.
 def register(username, password, password_confirm)
     if (password == password_confirm)
         password_digest = BCrypt::Password.create(password)
@@ -45,6 +50,9 @@ def register(username, password, password_confirm)
     end
 end
 
+# is the webbsite whre you chose your preferd genre to find books thath match the genre
+# @param [string] genre_is, when you chose your preferd genre it changes the genre id to the specific genre you have chosen
+
 def find_new_books(genre_id)
     db = SQLite3::Database.new('db/slutprojekt.db')
     db.results_as_hash = true
@@ -53,7 +61,8 @@ def find_new_books(genre_id)
 end
 
 
-
+# This is the webbsite wher based on yout schosen genre_id from the page before you will get all the books that matches with with the chosen genre_id
+# genre_id = session[:genre_id] the id thath every genre has, every genre has its own unique id
 def find_new_books_genres()
     genre_id = session[:genre_id] 
     db = SQLite3::Database.new('db/slutprojekt.db')
@@ -63,7 +72,10 @@ def find_new_books_genres()
 end
 
     
-
+# Attempts to login a user.
+#
+# @param [String] username The username inputted by the user.
+# @param [String] password The password inputted by the user.
 def login(username, password)  
     db = SQLite3::Database.new("db/slutprojekt.db") #?????scbpro????
     db.results_as_hash = true
@@ -71,6 +83,12 @@ def login(username, password)
     return result
 end
 
+
+# Checks if a user is already logged in.
+#
+# * :id [Integer] The ID of the user
+# @return [True] if user is already logged in.
+# @return [False] if user is not logged in.
 def already_logged_in()
     if session[:id] != nil
       return true
